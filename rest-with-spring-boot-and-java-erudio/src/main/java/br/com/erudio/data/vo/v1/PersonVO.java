928d3c3cbe46456e0ac50836ed55da993d1b64b3
JoaoAHaupt <joao.haupt@aluno.ifsp.edu.br>
2023-12-08 16:59:39 -0300

    Contente Negotiation

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
@@ -1,18 +1,25 @@
 package br.com.erudio.data.vo.v1;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serializable;
 import java.util.Objects;
 
-
+@JsonPropertyOrder({"identification", "first_name","last_name", "address","gender"})
 public class PersonVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-
+    @JsonProperty("identification")
     private Long id;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
+
     private String address;
+    
     private String gender;
 
     public PersonVO(){}
---
+++ rest-with-spring-boot-and-java-erudio/src/main/resources/db/migration/V2__Populate_Table_person.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO `person` VALUES
+(1,'Ana Luísa','Oliveira' ,'Lepo','Female'),
+(2,'Mateus','Ramos' ,'Zona Leste','Male'),
+(3,'Mariana','Megumi' ,'Lepoão','Male');
